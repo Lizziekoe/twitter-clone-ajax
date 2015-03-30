89c5ebe731cbfc5675e97208f098f0a93e94eda4
Jackie Fletcher and Lizzie Koehler <lizziefkoehler@gmail.com>
2015-03-30 10:25:50 -0700

    Initialize files and create posts class

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    render :index
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
+  end
+
+  private
+		def post_params
+			params.require(:post).permit(:content, :favorite)
+		end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+posts = Post.create([ {content: 'Happy Monday everybody!', favorite: false},
+  {content: 'Today is going to be fantastic!', favorite: false},
+  {content: 'Tomorrow is going to be fantastic!', favorite: false},
+  {content: 'Today is going to be fantastic!', favorite: false},
+  {content: 'Every day is a great day when programming! Especially with someone so good at learning like Lizzie! Hooray!'},
+  {content: 'Gardening is super great. Cannot wait to see the little sprouts growing! AND THEN I WILL EAT THEM MWAHAHAHA'},
+  {content: 'I am going to go to 7am yoga tomorrow at Yoga Union, and try out a two week pass for $25. What a great deal! Plesae hold me accountable so I get my butt out of bed in time. It will feel so super.'},
+   ])
